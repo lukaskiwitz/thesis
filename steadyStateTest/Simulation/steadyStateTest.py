918809cb9dcd2e15a25f6a58ca9b61465ffa5a87
lukaskiwitz <lukas.kiwitz@gmail.com>
2019-05-14 12:28:59 +0200

    new file:   steadyStateTest/Simulation/_settings.sqlite 	new file:   steadyStateTest/Simulation/steadyStateTest.py 	new file:   steadyStateTest/Simulation/steadyStateTest.xml 	new file:   steadyStateTest/Simulation/steadyStateTestSteppables.py 	new file:   steadyStateTest/Simulation/steadyStateTestSteppables.pyc 	new file:   steadyStateTest/steadyStateTest.cc3d

---
+++ steadyStateTest/Simulation/steadyStateTest.py
@@ -0,0 +1,34 @@
+
+import sys
+from os import environ
+from os import getcwd
+import string
+
+sys.path.append(environ["PYTHON_MODULE_PATH"])
+
+
+import CompuCellSetup
+
+
+sim,simthread = CompuCellSetup.getCoreSimulationObjects()
+        
+# add extra attributes here
+        
+CompuCellSetup.initializeSimulationObjects(sim,simthread)
+# Definitions of additional Python-managed fields go here
+        
+#Add Python steppables here
+steppableRegistry=CompuCellSetup.getSteppableRegistry()
+        
+from steadyStateTestSteppables import steadyStateTestSteppable
+steppableInstance=steadyStateTestSteppable(sim,_frequency=1)
+steppableRegistry.registerSteppable(steppableInstance)
+        
+
+from steadyStateTestSteppables import secretion
+instanceOfsecretion=secretion(_simulator=sim,_frequency=1)
+steppableRegistry.registerSteppable(instanceOfsecretion)
+
+CompuCellSetup.mainLoop(sim,simthread,steppableRegistry)
+        
+        
