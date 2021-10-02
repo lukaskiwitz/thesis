9aa15bdf6cc8b32aede6e9f4ff0b8541e0fc4990
Lukas <kiwitzlu@hu-berlin.com>
2021-10-02 08:24:54 +0200

    added parameter set api doc

--- docsrc/source/conf.py
+++ docsrc/source/conf.py
@@ -25,13 +25,14 @@ author = 'Lukas Kiwitz'
 # The full version, including alpha/beta/rc tags
 release = 'v0.1a'
 
-
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx_autodoc_typehints']
+
+autodoc_typehints = 'signature'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
--- docsrc/source/main.rst
+++ docsrc/source/main.rst
@@ -8,6 +8,7 @@ main package
     api/global_problem
     api/sim_container
     api/state_manager
+    api/parameter_pool
     api/post_processor
     api/my_plotter
 
