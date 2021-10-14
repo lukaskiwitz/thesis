dadc3d9faef612884c0d99d334f8ebe1fbe651b0
Lukas <kiwitzlu@hu-berlin.com>
2021-10-14 12:39:57 +0200

    added cell distance scan min example

---
+++ thesis/example_scripts/full/cell_distance_scan/parameters.py
@@ -0,0 +1,110 @@
+import getpass
+import os
+
+import numpy as np
+
+"""defines cytokines. Currently their parameters can be changed using the scan sample interface.
+"field_quantity" determines which boundary contitions interact with which fields, 
+the name is only used in IO/post processing."""
+
+cytokines = [
+    {
+        "name": "IL-2",
+        "field_quantity": "il2",
+        "k_on": 111.6,  # receptor binding constant 1/(nM*h),
+        "D": 10,  # Diffusion constant mu^2
+        "kd": 0.1  # cytokine decay in medium 1/h
+    }
+]
+
+"""Sets up cells types. T
+The first entry is the default cell type. There the "fraction" entry is meaningless.
+"""
+R_h = 1e4
+R_l = 0
+q = 10
+
+lol = [1, 2, 3]
+v = 1 / 10
+
+cell_types_dict = [
+    {"name": "default",
+     "mc": 0,
+     "fraction": 0,
+     "il2": {"R": 0, "q": 0, "Kc": 0.01, "ths": 0.01, "bc_type": "R_saturation"},
+     "misc": {"lol": lol},
+     "internal_solver": "RuleBasedSolver"
+     },
+    {"name": "sec",
+     "mc": 0,
+     "fraction": v / (v + 1),
+     "il2": {"R": R_l, "q": q, "Kc": 0.01, "ths": 0.01, "bc_type": "R_saturation"},
+     "misc": {"lol": lol},
+     "internal_solver": "RuleBasedSolver"
+     },
+    {"name": "abs",
+     "mc": 0,
+     "fraction": 1 / (v + 1),
+     "il2": {"R": R_h, "q": 0, "Kc": 0.01, "ths": 0.01, "bc_type": "R_saturation"},
+     "misc": {"lol": lol, "my_p": 1},
+     "internal_solver": "RuleBasedSolver"
+     }
+]
+
+boundary = [
+    {"name": "left_boundary",
+     "expr": "near(x[0],0)",
+     "il2": {"q": 0, "R": 0, "Kc": 0.01, "bc_type": "R_saturation"},
+     }
+]
+
+"""defines the variable aspects of the geometry. Unit is micro meters"""
+geometry = {
+    "margin": 20,  # margin around the cell grid
+    "distance": 20,  # distance between cell centers
+    "rho": 5,  # cell radius
+    "x_grid": 150,  # dimensions of the cell grid (edge length in um)
+    "y_grid": 150,
+    "z_grid": 150,  # comment out for single cell layer
+    "norm_area": 4 * np.pi * 5 ** 2  # area passed to bc function of outside boundary
+}
+
+"""
+parameters regarding meshing and fenics. unit_length_exponent is necessary for calculation concentrations. 
+-6  means the simulation is in micro meters.
+"""
+numeric = {
+    "linear_solver": "gmres",
+    "preconditioner": "hypre_amg",
+    "linear": False,  # use linear fenics solver
+    "krylov_atol": 1e-35,
+    "krylov_rtol": 1e-5,  # linear solver relative tolerance
+    "newton_atol": 1e-35,
+    "newton_rtol": 1e-5,  # newton method relative tolerance
+    "dofs_per_node": 15000,  # calc_boundary_values degrees of freedom per mpi node for pde solving
+    "max_mpi_nodes": os.cpu_count() / 2,  # max nodes for fenics solver
+    "cells_per_worker": 50,
+    "max_pool_size": os.cpu_count() / 2,  # max number of worker to extract boundary conditions at runtime
+    "min_char_length": 1,  # mesh resolution
+    "max_char_length": 5,  # mesh resolution
+    "unit_length_exponent": -6  # for concentration conversion
+}
+
+"""
+absolute paths
+"""
+if os.path.isdir("/extra2"):
+    extra = "extra2"
+else:
+    extra = "extra"
+
+user = getpass.getuser()
+model_name = "distance_scan"
+name = "cell-cell-distance"
+path = "/{extra}/{u}/{mn}/{n}/".format(u=user, n=name, mn=model_name, extra=extra)
+IMGPATH = path + "images/"
+
+"""
+relative to path; best to leave this alone
+"""
+ext_cache = r"../{mn}_ext_cache/".format(mn=model_name)
---
+++ thesis/example_scripts/full/cell_distance_scan/run.py
@@ -0,0 +1,104 @@
+import logging
+import os
+
+import numpy as np
+
+from parameters import cytokines, cell_types_dict, geometry, numeric, path, boundary
+
+os.environ["LOG_PATH"] = path
+LOG_PATH = os.environ.get("LOG_PATH") if os.environ.get("LOG_PATH") else "./"
+os.makedirs(LOG_PATH, exist_ok=True)
+logging.basicConfig(filename=LOG_PATH + "debug.log", level=logging.INFO, filemode="w",
+                    format='%(levelname)s::%(asctime)s %(message)s', datefmt='%I:%M:%S')
+
+os.environ["LOG_PATH"] = path
+
+import thesis.main.StateManager as StateManager
+from thesis.main.InternalSolver import InternalSolver
+from thesis.main.ParameterSet import ScannableParameter, MiscParameter
+from thesis.main.ScanContainer import ScanContainer, ScanDefintion, ScanType
+from thesis.scenarios.box_grid import setup, assign_fractions, distribute_receptors
+from scipy.stats import poisson
+
+
+class RuleBasedSolver(InternalSolver):
+    name = "RuleBasedSolver"
+
+    def on_type_change(self, p, replicat_index, entity=None):
+        pass
+
+    def step(self, t1, t2, dt, p, entity=None, **kwargs):
+
+        il2_threshold = p.get_physical_parameter("ths", "IL-2").get_in_post_unit()
+        il2 = p.get_physical_parameter("surf_c", "IL-2").get_in_post_unit()
+
+        rand = np.random.uniform(0, 1)
+
+        def hill(c, ths, invert=False):
+
+            l_max = 1
+            n = 4
+            if invert:
+                return l_max * (1 - (c ** n / (c ** n + ths ** n))) * dt
+            else:
+                return l_max * ((c ** n / (c ** n + ths ** n))) * dt
+
+        il2_cdf = 1 - poisson.cdf(k=1, mu=hill(il2, il2_threshold, invert=True))
+
+        if entity.type_name == "default":
+            if il2_cdf > rand:
+                entity.change_type = "sec"
+        return p
+
+
+"""Setup/Simulation"""
+
+"""
+setting filepath for simulation results. This is setup so that it works on the itb computers.
+If ext_cache = "" the mesh will be cached for each field separately
+"""
+
+"""Setting up a parameters scan now has a object oriented interface. This is the container class"""
+scan_container = ScanContainer()
+
+"""the setup function is defined in an external file. It builds the SimContainer for this simulation.
+The variable aspects are passed as list "cytokines, cell_types, geometry and numeric. 
+These are imported as modules and can be modified in parameters.py """
+
+scenario = setup(cytokines, cell_types_dict, boundary, geometry, numeric)
+
+"""scan of cell-cell-distance"""
+distance = ScannableParameter(MiscParameter("distance", 20), lambda x, v: v + 10)
+margin = ScannableParameter(MiscParameter("margin", 20), lambda x, v: v)
+scan_space = np.linspace(5, 15, 5)
+distance_def = ScanDefintion(distance, "geometry", scan_space, ScanType.GLOBAL)
+margin_def = ScanDefintion(margin, "geometry", scan_space, ScanType.GLOBAL)
+
+"""add parameter scans to scan_container"""
+scan_container.add_single_parameter_scan([distance_def, margin_def], scan_name="distance", remesh_scan_sample=True)
+
+
+def update_state(sc, replicat_index):
+    assign_fractions(sc, replicat_index)
+    sc.apply_type_changes(replicat_index)
+    entity_list = sc.entity_list
+
+    for type_name in sc.get_number_of_entities().keys():
+        distribute_receptors(entity_list, replicat_index, type_name)
+
+
+def pre_replicat(sim_container, time_index, replicat_index, t, T):
+    update_state(sim_container, replicat_index)
+
+
+stMan = StateManager.StateManager(path)
+stMan.scenario = scenario
+
+stMan.marker_lookup = {"default": 1, "sec": 2, "abs": 3}  # labels to apper in marker function; 0 denotes background
+
+stMan.compress_log_file = True
+stMan.pre_replicat = pre_replicat
+
+stMan.scan_container = scan_container
+stMan.T = [0, 1]
+stMan.run()
