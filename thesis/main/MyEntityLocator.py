7361dd78b21ac43607c00221c1f156b7c6348f25
brunner-itb <patrickbrunner91@gmail.com>
2021-12-17 12:52:51 +0100

    KDTree random cell placement

--- thesis/main/MyEntityLocator.py
+++ thesis/main/MyEntityLocator.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 
 import numpy as np
-from scipy.spatial import KDTree, distance_matrix
+from scipy.spatial import cKDTree, distance_matrix
 
 from thesis.main.Entity import Entity, Cell
 from thesis.main.EntityType import EntityType, CellType
@@ -131,6 +131,21 @@ class MyRandomCellLocator(MyCellGridLocator):
         """
         pass
 
+    def random_3D_unit_vector(self):
+        """
+        Generates a random 3D unit vector (direction) with a uniform spherical distribution
+        Algo from http://stackoverflow.com/questions/5408276/python-uniform-spherical-distribution
+        :return:
+        """
+        phi = np.random.uniform(0, np.pi * 2)
+        costheta = np.random.uniform(-1, 1)
+
+        theta = np.arccos(costheta)
+        x = np.sin(theta) * np.cos(phi)
+        y = np.sin(theta) * np.sin(phi)
+        z = np.cos(theta)
+        return np.array([x, y, z])
+
     def get_entity_list(self, cell_type: CellType, global_p: ParameterSet) -> [Cell]:
         assert issubclass(type(cell_type), CellType)
         r = cell_type.p.get_physical_parameter("rho", "rho").get_in_sim_unit()
@@ -161,49 +176,26 @@ class MyRandomCellLocator(MyCellGridLocator):
 
         positions = np.vstack(list(zip(x.ravel(), y.ravel(), z.ravel())))
 
-        dist_m = distance_matrix(positions, positions)
-        dist_m[np.tril_indices(len(dist_m[0]), k=0)] = None
-
         iterations = 1000
 
         for i in range(iterations):
-            too_close = np.argwhere(dist_m < (rho + penalty) * 2)
-            for tc in too_close:
-                cell_1 = np.round(positions[tc[0]], 2)
-                cell_2 = np.round(positions[tc[1]], 2)
-                c_c_v = ((cell_2 - cell_1) / 2) * 0.1
-                positions[tc[0]] += - c_c_v
-                positions[tc[1]] += + c_c_v
+            len_neighbors = 0
+            tree = cKDTree(positions)
+            for j in range(len(positions)):
+                neighbors = tree.query(positions[j], 2, distance_upper_bound = (rho + penalty) * 2, n_jobs = 1)[1][1:]
+                neighbors = neighbors[neighbors != len(positions)]
+                length = len(neighbors)
+                len_neighbors += length
+                if length != 0:
+                    cell_1 = np.mean(positions[neighbors], axis=0)
+                    cell_2 = positions[j]
+                    factor = 1.01 if length > 4 else 0.1
+                    c_c_v = ((cell_2 - cell_1) / 2) * factor
+                    positions[j] += + c_c_v
 
-            dist_m = distance_matrix(positions, positions)
-            dist_m[np.tril_indices(len(dist_m[0]), k=0)] = None
-
-            too_close = np.argwhere(dist_m < (rho + penalty) * 2)
-            if len(too_close) == 0:
-                print(i)
+            if len_neighbors == 0:
                 break
 
-
-
-
-        dist_list = []
-        for coord in range(len(dist_m)):
-            # dist_list.append(np.nanmin(dist_m[coord]))
-            dist_list.append(np.sort(dist_m[coord])[6])
-
-        # from mpl_toolkits.mplot3d import Axes3D
-        # import matplotlib.pyplot as plt
-        # plt.hist(dist_list, bins=46)
-        # plt.axvline(np.nanmean(dist_list), 0, 130, color="orange")
-        # plt.axvline(20, 0, 130, color="orange")
-        # plt.xlim((0, None))
-        # plt.show()
-        # #
-        # fig = plt.figure()
-        # ax = fig.add_subplot(projection='3d')
-        # ax.scatter(positions[:, 0], positions[:, 1], positions[:, 2], marker=".", s=20)
-        # # ax.scatter(x, y, z, marker="o")
-        # plt.show()
 
         return positions
 
